ecfa387de9f27fa98b3d5a6fd8675e8a4de10886
Seungmin123 <dltmdals147@gmail.com>
2023-02-02 17:25:28 +0900

    add responseEntity ExceptionHandler

--- src/main/java/com/whalee/universe/config/exception/ExceptionAdvice.java
+++ src/main/java/com/whalee/universe/config/exception/ExceptionAdvice.java
@@ -2,12 +2,15 @@ package com.whalee.universe.config.exception;
 
 import com.whalee.universe.common.enums.exceptions.CommonExceptionCode;
 import com.whalee.universe.common.enums.exceptions.ErrorCode;
-import com.whalee.universe.common.enums.exceptions.ErrorResponse;
 import com.whalee.universe.common.response.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
@@ -37,5 +40,13 @@ public class ExceptionAdvice extends ResponseEntityExceptionHandler {
 
     private CommonResponse<Object> handleExceptionInternal(ErrorCode errorCode) {
         return CommonResponse.builder().code(errorCode.getHttpStatus().toString()).message(errorCode.getMessage()).build();
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatus status, WebRequest webRequest){
+        String messageCode = e.getBindingResult()
+                .getFieldErrors()
+                .stream().findFirst().get().getDefaultMessage();
+        return new ResponseEntity<>(messageCode, new HttpHeaders(), HttpStatus.BAD_GATEWAY);
     }
 }
